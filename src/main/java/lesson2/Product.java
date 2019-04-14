418beaea87af05352999c003acadc007112eca64
Oleg_hos <ooleguner@gmail.com>
2019-04-14 20:48:54 +0300

    Lesson2_2

---
+++ src/main/java/lesson2/Product.java
@@ -0,0 +1,53 @@
+package lesson2;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+
+    private long Id;
+    private String name;
+    private String description;
+    private int price;
+
+    
+    @Id
+    @SequenceGenerator(name = "PR_SEQ", sequenceName = "PRODUCT_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PR_SEQ")
+    @Column(name = "ID")
+    public long getId() {
+        return Id;
+    }
+
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    @Column(name = "PRICE")
+    public int getPrice() {
+        return price;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
